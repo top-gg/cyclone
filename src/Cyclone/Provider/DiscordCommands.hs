4eb3d52ba920d8c1e1bf67b41da6db3a0008a809
xetera <mythologyali@gmail.com>
2021-10-25 05:57:22 +0300

    updated matcher for Discord-MusicBot

---
+++ src/Cyclone/Provider/DiscordCommands.hs
@@ -0,0 +1,8 @@
+module Cyclone.Provider.DiscordCommands where
+
+import Discord
+import Discord.Types
+
+-- | Checks to see if the message being replied to is a valid response
+commandCheckReplyMessage :: Message -> DiscordHandler ()
+commandCheckReplyMessage = undefined
